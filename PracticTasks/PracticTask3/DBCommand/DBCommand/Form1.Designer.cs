63796f57655d3ddc6871acfacaee5354385e7c74
EgorAlekseev <54812985+EgorAlekseev@users.noreply.github.com>
2020-07-15 13:34:47 +0300

    Add files

--- PracticTasks/PracticTask3/DBCommand/DBCommand/Form1.Designer.cs
+++ PracticTasks/PracticTask3/DBCommand/DBCommand/Form1.Designer.cs
@@ -35,6 +35,15 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.sqlCommand2 = new System.Data.SqlClient.SqlCommand();
+            this.button4 = new System.Windows.Forms.Button();
+            this.sqlCommand3 = new System.Data.SqlClient.SqlCommand();
+            this.button5 = new System.Windows.Forms.Button();
+            this.CityTextBox = new System.Windows.Forms.TextBox();
+            this.sqlCommand4 = new System.Data.SqlClient.SqlCommand();
+            this.button6 = new System.Windows.Forms.Button();
+            this.CategoryNameTextBox = new System.Windows.Forms.TextBox();
+            this.OrdYearTextBox = new System.Windows.Forms.TextBox();
+            this.sqlCommand5 = new System.Data.SqlClient.SqlCommand();
             this.SuspendLayout();
             // 
             // sqlConnection1
@@ -93,11 +102,92 @@
             this.sqlCommand2.CommandType = System.Data.CommandType.StoredProcedure;
             this.sqlCommand2.Connection = this.sqlConnection1;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(649, 33);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 37);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Создание таблицы";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // sqlCommand3
+            // 
+            this.sqlCommand3.Connection = this.sqlConnection1;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(787, 33);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 42);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Запрос с параметром";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // CityTextBox
+            // 
+            this.CityTextBox.Location = new System.Drawing.Point(787, 97);
+            this.CityTextBox.Name = "CityTextBox";
+            this.CityTextBox.Size = new System.Drawing.Size(90, 20);
+            this.CityTextBox.TabIndex = 6;
+            this.CityTextBox.Text = "London";
+            // 
+            // sqlCommand4
+            // 
+            this.sqlCommand4.CommandText = "SELECT CustomerID, CompanyName, City FROM Customers WHERE City = @City";
+            this.sqlCommand4.Connection = this.sqlConnection1;
+            this.sqlCommand4.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@City", System.Data.SqlDbType.NVarChar, 15, "City")});
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(941, 33);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 41);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Процедура с параметром";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // CategoryNameTextBox
+            // 
+            this.CategoryNameTextBox.Location = new System.Drawing.Point(941, 96);
+            this.CategoryNameTextBox.Name = "CategoryNameTextBox";
+            this.CategoryNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.CategoryNameTextBox.TabIndex = 8;
+            this.CategoryNameTextBox.Text = "Beverages";
+            // 
+            // OrdYearTextBox
+            // 
+            this.OrdYearTextBox.Location = new System.Drawing.Point(941, 144);
+            this.OrdYearTextBox.Name = "OrdYearTextBox";
+            this.OrdYearTextBox.Size = new System.Drawing.Size(100, 20);
+            this.OrdYearTextBox.TabIndex = 9;
+            this.OrdYearTextBox.Text = "1997";
+            // 
+            // sqlCommand5
+            // 
+            this.sqlCommand5.CommandText = "SalesByCategory";
+            this.sqlCommand5.CommandType = System.Data.CommandType.StoredProcedure;
+            this.sqlCommand5.Connection = this.sqlConnection1;
+            this.sqlCommand5.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@RETURN_VALUE", System.Data.SqlDbType.Int, 4, System.Data.ParameterDirection.ReturnValue, false, ((byte)(0)), ((byte)(0)), "", System.Data.DataRowVersion.Current, null),
+            new System.Data.SqlClient.SqlParameter("@CategoryName", System.Data.SqlDbType.NVarChar, 15),
+            new System.Data.SqlClient.SqlParameter("@OrdYear", System.Data.SqlDbType.NVarChar, 4)});
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 561);
+            this.ClientSize = new System.Drawing.Size(1078, 561);
+            this.Controls.Add(this.OrdYearTextBox);
+            this.Controls.Add(this.CategoryNameTextBox);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.CityTextBox);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.ResultsTextBox);
@@ -118,6 +208,15 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Data.SqlClient.SqlCommand sqlCommand2;
+        private System.Windows.Forms.Button button4;
+        private System.Data.SqlClient.SqlCommand sqlCommand3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox CityTextBox;
+        private System.Data.SqlClient.SqlCommand sqlCommand4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox CategoryNameTextBox;
+        private System.Windows.Forms.TextBox OrdYearTextBox;
+        private System.Data.SqlClient.SqlCommand sqlCommand5;
     }
 }
 
--- PracticTasks/PracticTask3/DBCommand/DBCommand/Form1.cs
+++ PracticTasks/PracticTask3/DBCommand/DBCommand/Form1.cs
@@ -114,5 +114,80 @@ namespace DBCommand
                 }
             }
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (sqlConnection1) 
+            { 
+                sqlCommand3.CommandText =   "CREATE TABLE SalesPersons " + "(" + "[SalesPersonID] [int] IDENTITY(1,1) NOT NULL, " + 
+                                            "[FirstName] [nvarchar](50) NULL, " + "[LastName] [nvarchar](50) NULL)"; 
+                try 
+                {   sqlConnection1.Open();
+                    sqlCommand3.ExecuteNonQuery(); 
+                    MessageBox.Show("Таблица SalesPersons создана"); 
+                } 
+                catch (SqlException ex) 
+                { 
+                    MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+                } 
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            StringBuilder results = new StringBuilder(); 
+            try
+            {
+                sqlCommand4.Parameters["@City"].Value = CityTextBox.Text; 
+                sqlConnection1.Open(); 
+                SqlDataReader reader = sqlCommand4.ExecuteReader();
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++) 
+                    { 
+                        results.Append(reader[i].ToString() + "\t"); 
+                    }
+                    results.Append(Environment.NewLine);
+                }
+                ResultsTextBox.Text = results.ToString();
+            }
+            catch (SqlException ex) 
+            { 
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+            }
+            finally 
+            { 
+                sqlConnection1.Close(); 
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            StringBuilder results = new StringBuilder(); 
+            try
+            {
+                sqlCommand5.Parameters["@CategoryName"].Value = CategoryNameTextBox.Text; 
+                sqlCommand5.Parameters["@OrdYear"].Value = OrdYearTextBox.Text;
+                sqlConnection1.Open(); 
+                SqlDataReader reader = sqlCommand5.ExecuteReader();
+                while (reader.Read()) 
+                { 
+                    for (int i = 0; i < reader.FieldCount; i++) 
+                    { 
+                        results.Append(reader[i].ToString() + "\t"); 
+                    } 
+                    results.Append(Environment.NewLine); 
+                }
+                ResultsTextBox.Text = results.ToString();
+            }
+            catch (SqlException ex) 
+            { 
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+            }
+            finally 
+            { 
+                sqlConnection1.Close(); 
+            }
+        }
     }
 }
